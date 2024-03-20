29260cfa8054fef70fb783ba234ff743ea38f85e
Tomasz Jasinski <tomjas@gmail.com>
2024-03-20 12:34:34 +0100

    Initial commit

---
+++ src/main/java/com/database/integration/DatabaseIntegrationConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.database.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseIntegrationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseIntegrationConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/database/integration/DatabaseIntegrationConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseIntegrationConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
